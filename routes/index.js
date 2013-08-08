a6b167ac9fdcf1d99618d7303cc01ee989d66b65
deanlai <sean.j.lai@gmail.com>
2013-08-07 23:31:16 -0700

    added minisites to navigation and abstracted description model for pages

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,33 @@
 // MAIN
 // GET home page.
  exports.index = function(req, res){
-  res.render('index', { title: 'Built by Sean: Portfolio of Sean Lai', path: req.path });
+  res.render('index', {
+    title: 'Built by Sean: Portfolio of Sean Lai',
+    description: 'The Portfolio of Sean Lai, Portland OR. A collection of art/design projects, documentation of places been, and professional work by Sean Lai.',
+    path: req.path });
 };
 
 // GET /work page
 exports.work = function(req, res){
-  res.render('work', { title: 'Built by Sean: Portfolio of Sean Lai > Work', path: req.path});
+  res.render('work', {
+    title: 'Built by Sean: Portfolio of Sean Lai > Work',
+    description: 'A selection of works and have-dones of Sean Lai, Portland OR',
+    path: req.path});
 };
 
 // GET /about page
 exports.about = function(req, res){
-  res.render('about', { title: 'Built by Sean: Portfolio of Sean Lai > About', path: req.path});
+  res.render('about', {
+    title: 'Built by Sean: Portfolio of Sean Lai > About',
+    description: 'General information about me, with a portrait thrown in for good measure.',
+    path: req.path});
 };
 
 // GET /contact page
 exports.contact = function(req, res){
-  res.render('contact', { title: 'Built by Sean: Portfolio of Sean Lai > Contact', path: req.path});
+  res.render('contact', {
+    title: 'Built by Sean: Portfolio of Sean Lai > Contact',
+    description: 'Contact information for Sean Lai, Portland OR',
+    path: req.path});
 };
 
--- routes/minisites.js
+++ routes/minisites.js
@@ -1,13 +1,22 @@
 // GET /minisites home page
 
 exports.index = function(req, res){
-    res.render('mini-index', { title: 'Minisites: Web development experiments by Sean Lai', path: req.path});
+    res.render('mini-index', {
+        title: 'Minisites: Web development experiments by Sean Lai',
+        description: 'A collection of miniture websites, for utility, experimentation, and general whimsical WevDev-ness',
+        path: req.path});
 };
 
 exports.coin = function(req, res){
-    res.render('minisites/coin', { title: 'Flip a coin!', path: req.path});
+    res.render('minisites/coin', {
+        title: 'Flip a coin!',
+        description: 'A simple coin flipping application for decisions of all kinds',
+        path: req.path});
 };
 
 exports.energy = function(req, res){
-    res.render('minisites/energy', { title: 'How much energy?', path: req.path});
+    res.render('minisites/energy', {
+        title: 'How much energy?',
+        description: 'A comparison of some common but often startlingly different energy quantities',
+        path: req.path});
 };
